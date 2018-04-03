589ea1172e343e9916b3d5fa50e463ccce922721
Andrew <andreiduffy@gmail.com>
2018-04-03 09:04:56 +0300

    home ready 1.4

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 	<meta property="og:image" content="/img/og.jpg">
 	<!-- og -->
 
-	<link href="css/main.css?v=8" rel="stylesheet">
+	<link href="css/main.css?v=9" rel="stylesheet">
 </head>
 
 <body>
