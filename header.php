e6787386ddb2b26232b4b6475d5541f41ed01deb
Andrew <andreiduffy@gmail.com>
2018-05-06 10:11:34 +0300

    sync

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 	<meta property="og:image" content="/img/og.jpg">
 	<!-- og -->
 
-	<link href="css/main.css?v=21" rel="stylesheet">
+	<link href="css/main.css?v=22" rel="stylesheet">
 </head>
 
 <body>
