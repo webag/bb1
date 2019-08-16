4a1e616811d28661bb5634766f16f6c5fffe45b2
Andrew <andreiduffy@gmail.com>
2019-08-16 13:12:18 +0300

    index langs

---
+++ index-en.php
@@ -0,0 +1,161 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+
+	<title>BB1</title>
+
+	<!-- favocons -->
+	<link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+	<link rel="manifest" href="favicons/manifest.json">
+	<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="favicons/favicon.ico">
+	<meta name="msapplication-config" content="favicons/browserconfig.xml">
+	<meta name="theme-color" content="#000000">
+	<!-- favocons -->
+
+	<!-- og -->
+	<meta property="og:title" content="BB1">
+	<meta property="og:description" content="BB1">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="/img/og.jpg">
+	<!-- og -->
+
+	<link href="css/main.css?v=23" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+<header class="s-top-panel compensate-for-scrollbar">
+	<div class="container">
+
+		<div class="top-panel-logo">
+			<a href="/" title="На главную">
+				<img src="/img/logo-mini.png" alt="logotype" width="75">
+			</a>
+		</div>
+
+		<div class="top-panel-actions">
+			<div class="top-langs">
+				<a href="/index-lv.php">LV</a>
+				<a href="/index-en.php" class="active">EN</a>
+				<a href="/index-ru.php">RU</a>
+			</div>
+		</div>
+
+	</div>
+</header>
+
+
+
+<section class="s-top-logo">
+	<div class="container">
+		<a href="/" title="На главную">
+			<img src="/img/logo.png" alt="logotype" width="150">
+		</a>
+		<div class="s-top-logo__descr">Accessories are the part of life</div>
+	</div>
+</section>
+
+
+<section>
+	<img src="/img/index-new/banner.jpg" alt="" class="img-responsive">
+</section>
+
+
+<section class="s-contacts-new">
+	<div class="container">
+		<div class="row">
+			<div class="grid-6 grid-12_s">
+				<div class="contacts-new-map">
+					<img src="/img/index-new/map.jpg" alt="">
+				</div>
+			</div>
+			<div class="grid-6 grid-12_s">
+				<div class="contacts-new">
+					<div class="contacts-new__city">Riga, Latvia</div>
+					<div class="contacts-new__title">SIA BB1 SHOP</div>
+					<p>ВВ1 oficiālais pārstāvis Latvijā</p>
+					<p>Veikalа BB1SHOP adrese:<br>
+						Tirdzniecības centrs "Domina Shopping”<br>
+						Ieriķu iela 3, Riga, Latvia</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="s-title-panel">
+	<div class="container">
+		<p class="h1">Online shop BB1</p>
+	</div>
+</section>
+
+
+<section class="s-home-full-promos">
+	<div class="container">
+
+		<div class="promo-full">
+			<div class="promo-full__content">
+				<p class="extra-big">BB1 BAGS</p>
+				<p>Sieviešu un vīriešu ādas somu kolekcija</p>
+				<div class="promo-full__actions promo-full__actions--single">
+					<a href="#" class="btn">Skatīties kolekciju oficiālajā mājas lapā BB1</a>
+				</div>
+			</div>
+			<div class="promo-full__img promo-full__img--new">
+				<img src="/img/index-new/promo-1.jpg" alt="">
+			</div>
+		</div>
+
+		<div class="promo-full">
+			<div class="promo-full__img promo-full__img--new">
+				<img src="/img/index-new/promo-2.jpg" alt="">
+			</div>
+			<div class="promo-full__content">
+				<p class="extra-big">BB1 BELTS</p>
+				<p>Sieviešu un vīriešu premium ādas jostu kolekcija</p>
+				<div class="promo-full__actions promo-full__actions--single">
+					<a href="#" class="btn">Skatīties kolekciju oficiālajā mājas lapā BB1</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</section>
+
+
+<footer class="s-footer">
+	<div class="container">
+		<div class="footer-simple">
+			<img src="/img/index-new/white-logo.png" alt="logo" class="footer-simple__logo">
+			<div class="footer-simple__text">
+				Copyright © 2003-2019    BB1 USA Inc.
+			</div>
+			<div class="social-links">
+				<a href="#" target="_blank"><i class="i-vk"></i></a>
+				<a href="#" target="_blank"><i class="i-fb"></i></a>
+				<a href="#" target="_blank"><i class="i-insta"></i></a>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+</body>
+</html>
+
